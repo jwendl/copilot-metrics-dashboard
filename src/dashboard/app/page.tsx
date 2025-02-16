2c0bc23f3a03fd4f22358c2e58b22a4732362140
Justin Wendlandt <jwendl@hotmail.com>
2025-02-15 16:27:14 -0800

    Updating accounts

--- src/dashboard/app/page.tsx
+++ src/dashboard/app/page.tsx
@@ -9,7 +9,7 @@ export const dynamic = "force-dynamic";
 export default async function Home(props: IProps) {
   let id = "initial-dashboard";
   const session = await getServerSession();
-  const validUserEmails: string[] = ["juswen@microsoft.com", "altsang@microsoft.com", "katerayner@microsoft.com", "philcousins@microsoft.com", "dermille@microsoft.com", "phlucas@microsoft.com", "relghazi@microsoft.com", "macook@microsoft.com"];
+  const validUserEmails: string[] = ["juswen@microsoft.com", "altsang@microsoft.com", "katerayner@microsoft.com", "philcousins@microsoft.com", "dermille@microsoft.com", "phlucas@microsoft.com", "relghazi@microsoft.com", "macook@microsoft.com", "markmc@microsoft.com"];
 
   if (!session) {
     redirect('/api/auth/signin');
